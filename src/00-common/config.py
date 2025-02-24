98eccfd90932a47b91e36754ee16bba591ec8281
Sean Sullivan <sean.m.a.sullivan@icloud.com>
2025-02-23 19:49:23 -0500

    moving api token to config file

---
+++ src/00-common/config.py
@@ -0,0 +1,2 @@
+# filepath: /Users/seansullivan/Documents/LinkedIn%20Learning/quantum-computing-fundamentals-2833097-main/src/config.py
+IBM_QUANTUM_API_TOKEN = '0c66e11441d98f0444d55bec0650f6a92bc7ac016baa258aa1343b8152f13fb0a10ea612064381ae3b411e78f90c8c763a1545aa5400278ed3d6e49822b929b0'
